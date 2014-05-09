16b213dd211da44953ca4a7c4db6128f6e1f5486
duong2009 <phamduong2009@gmail.com>
2014-05-09 19:17:57 +1000

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,6 @@
+install.packages("sqldf")
+library(sqldf)
+
 mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
 myData <- read.csv.sql("household_power_consumption.txt", sql = mySql, sep =";")
 
